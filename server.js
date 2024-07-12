553910d81bfc8309be3a06b37cc78779e9c2f8c8
kumaraswamy-alpha <kumaraswamy.malga@accelyzei.com>
2024-07-12 12:11:30 +0530

    intial commit

---
+++ server.js
@@ -0,0 +1,39 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get("/name", (req, res) => {
+  res.send("Hello World!");
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log("Server is running on Port", PORT);
+});
+
+app.get("/user", (req, res) => {
+  res.send("You are getting a user data back!");
+});
+
+app.get("/user", (req, res) => {
+  console.log("This is never going to run");
+  res.send("You cannot get this response back");
+});
+
+app.post("/user", (req, res) => {
+  console.log(req.body);
+  res.send("We created a user with firstname of " + req.body.firstName);
+});
+
+app.delete("/user", (req, res) => {
+  console.log(req.body);
+  res.send("We deleted a user with firstname of " + req.body.firstName);
+});
+
+app.put("/user", (req, res) => {
+  console.log(req.body);
+  res.send("We updated a user with firstname of " + req.body.firstName);
+});
